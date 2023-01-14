2e40a9406fa5fa08cc0050c123c982eecd305087
bill <bill2021@anyong.com.tw>
2023-01-14 16:10:36 +0800

    新增market頁面

---
+++ src/pages/market/market.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">Market</div>
+</template>
+<script>
+export default {
+    name: "MarketComponent",
+    props: {},
+    data() {
+        return {};
+    },
+    created() {},
+    methods: {},
+    filters: {},
+};
+</script>
+<style scoped lang="scss" src="./market.scss"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HomeComponent from "../pages/index/index.vue";
+import MarketComponent from "../pages/market/market.vue";
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
             path: "/",
             name: "HomeComponent",
             component: HomeComponent,
+        },
+        {
+            path: "/market",
+            name: "MarketComponent",
+            component: MarketComponent,
         },
     ],
 });
