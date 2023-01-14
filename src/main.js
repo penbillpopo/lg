e4b32dac8fabbd346690ab1b0b9c01e77058c7e7
bill <bill2021@anyong.com.tw>
2023-01-14 15:59:36 +0800

    新增第一頁bg

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./assets/scss/common/_reset.scss";
 
 Vue.config.productionTip = false;
 
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,200 @@
 <template>
-    <div>123</div>
+    <div class="container">
+        <div class="section1">
+            <div class="flex-box">
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/01.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/02.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/03.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/04.jpg`)"
+                    alt=""
+                />
+            </div>
+            <div class="flex-box mobile">
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/01.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/02.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/03.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/04.jpg`)"
+                    alt=""
+                />
+            </div>
+        </div>
+        <div class="section2">
+            <div
+                class="bg"
+                :style="{
+                    'background-image':
+                        'url(' + require(`@/assets/img/index1-1/bg.jpg`) + ')',
+                }"
+            ></div>
+        </div>
+        <div class="section3">
+            <div class="flex-box">
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/01.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/02.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/03.jpg`)"
+                    alt=""
+                />
+                <img
+                    class="col25"
+                    :src="require(`@/assets/img/index1-0/04.jpg`)"
+                    alt=""
+                />
+            </div>
+        </div>
+        <div class="section4">
+            <div
+                class="bg"
+                :style="{
+                    'background-image':
+                        'url(' + require(`@/assets/img/index1-3/bg.png`) + ')',
+                }"
+            ></div>
+        </div>
+        <div class="section5">
+            <div
+                class="bg"
+                :style="{
+                    'background-image':
+                        'url(' + require(`@/assets/img/index1-4/bg.png`) + ')',
+                }"
+            ></div>
+        </div>
+        <div class="section6">
+            <div class="question-box">
+                <div
+                    class="question"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            require(`@/assets/img/index1-5/1-5-L.png`) +
+                            ')',
+                    }"
+                ></div>
+                <div
+                    class="answer"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            require(`@/assets/img/index1-5/1-5-R.png`) +
+                            ')',
+                    }"
+                ></div>
+            </div>
+        </div>
+        <div class="section7">
+            <div class="question-box">
+                <div
+                    class="question"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            require(`@/assets/img/index1-6/1-6-L.png`) +
+                            ')',
+                    }"
+                ></div>
+                <div
+                    class="answer"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            require(`@/assets/img/index1-6/1-6-R.png`) +
+                            ')',
+                    }"
+                ></div>
+            </div>
+        </div>
+        <div class="section8">
+            <div class="question-box">
+                <div
+                    class="question"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            require(`@/assets/img/index1-7/1-7-L.png`) +
+                            ')',
+                    }"
+                ></div>
+                <div
+                    class="answer"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            require(`@/assets/img/index1-7/1-7-R.png`) +
+                            ')',
+                    }"
+                ></div>
+            </div>
+        </div>
+        <div class="section9">
+            <div class="question-box">
+                <div
+                    class="question"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            require(`@/assets/img/index1-8/1-8-L.png`) +
+                            ')',
+                    }"
+                ></div>
+                <div
+                    class="answer"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            require(`@/assets/img/index1-8/1-8-R.png`) +
+                            ')',
+                    }"
+                ></div>
+            </div>
+        </div>
+        <div class="section10">
+            <div
+                class="bg"
+                :style="{
+                    'background-image':
+                        'url(' + require(`@/assets/img/index1-9/bg.png`) + ')',
+                }"
+            ></div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
@@ -7,6 +202,14 @@ export default {
     props: {
         msg: String,
     },
+    data() {
+        return {
+            sectionOneImgs: ["@/assets/img/index1-0/01.jpg"],
+        };
+    },
+    created() {},
+    methods: {},
+    filters: {},
 };
 </script>
 <style scoped lang="scss" src="./index.scss"></style>
