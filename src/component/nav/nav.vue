3d9f1f3bef9710816aad97e185f28813dc4a0066
bill <bill2021@anyong.com.tw>
2023-01-19 04:14:02 +0800

    動態修正

---
+++ src/component/nav/nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="nav">
+        <img class="logo" :src="require(`@/assets/img/logo.png`)" alt="" />
+    </div>
+</template>
+<script>
+export default {
+    name: "navComponent",
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+<style scoped lang="scss" src="./nav.scss"></style>
