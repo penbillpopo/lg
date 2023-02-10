eacb0fe2df75e1472535b1e029d1950793b8c6f2
bill <bill2021@anyong.com.tw>
2023-02-10 10:18:54 +0800

    移除loading

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@ import { Fancybox } from "@fancyapps/ui";
 import { scrollRevealMixin } from "./mixin/scrollReveal";
 import "./assets/scss/common/_reset.scss";
 import "@fancyapps/ui/dist/fancybox.css";
-import { imgsPreloader } from "./extension/imgPreloader.js";
-import imgPreloaderList from "./extension/imgPreloaderList.js";
 import VueAnalytics from "vue-analytics";
+// import { imgsPreloader } from "./extension/imgPreloader.js";
+// import imgPreloaderList from "./extension/imgPreloaderList.js";
 
 const isProd = false;
 
 (async () => {
-    await imgsPreloader(imgPreloaderList);
-    document.querySelector(".loading").style.display = "none";
+    // await imgsPreloader(imgPreloaderList);
+    // document.querySelector(".loading").style.display = "none";
     Vue.config.productionTip = false;
     Vue.use(VueAnalytics, {
         id: "G-T9BMWQQ6D0",
