f209cd42d40d7f3786856f68a5df38e43ad4ef58
bill <bill2021@anyong.com.tw>
2023-02-23 00:02:19 +0800

    解除ga註解

--- src/main.js
+++ src/main.js
@@ -5,27 +5,17 @@ import { Fancybox } from "@fancyapps/ui";
 import { scrollRevealMixin } from "./mixin/scrollReveal";
 import "./assets/scss/common/_reset.scss";
 import "@fancyapps/ui/dist/fancybox.css";
-import VueAnalytics from "vue-analytics";
-// import { imgsPreloader } from "./extension/imgPreloader.js";
-// import imgPreloaderList from "./extension/imgPreloaderList.js";
-
-const isProd = false;
+import VueGtag from "vue-gtag";
 
 (async () => {
-    // await imgsPreloader(imgPreloaderList);
-    // document.querySelector(".loading").style.display = "none";
     Vue.config.productionTip = false;
-    Vue.use(VueAnalytics, {
-        id: "G-T9BMWQQ6D0",
-        router,
-        debug: {
-            enabled: !isProd,
-            sendHitTask: isProd,
+    Vue.use(
+        VueGtag,
+        {
+            config: { id: "G-T9BMWQQ6D0" },
         },
-        autoTracking: {
-            pageviewOnLoad: false,
-        },
-    });
+        router
+    );
     new Vue({
         router,
         Fancybox,
